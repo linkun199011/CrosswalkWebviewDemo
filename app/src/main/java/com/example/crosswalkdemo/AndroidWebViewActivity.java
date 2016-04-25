8dc5e3e78fa11e66eb1354bb154b9b023728fc3d
LinKun <linkun199011@163.com>
2016-04-25 12:07:30 +0800

    LinKun:add auto loading

--- app/src/main/java/com/example/crosswalkdemo/AndroidWebViewActivity.java
+++ app/src/main/java/com/example/crosswalkdemo/AndroidWebViewActivity.java
@@ -1,10 +1,12 @@
 package com.example.crosswalkdemo;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * @author LinKun EmployeeID:151750
@@ -22,7 +24,7 @@ public class AndroidWebViewActivity extends Activity {
         Bundle bundle = getIntent().getExtras();
         mUrl = bundle.getString("mUrl");
         System.out.println(">>> URL is = " + mUrl);
-        mWebView=(WebView)findViewById(R.id.xwalkWebView);
+        mWebView = (WebView) findViewById(R.id.xwalkWebView);
         //支持javascript
         mWebView.getSettings().setJavaScriptEnabled(true);
         // 设置可以支持缩放
@@ -34,16 +36,31 @@ public class AndroidWebViewActivity extends Activity {
         //自适应屏幕
         mWebView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
         mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);// 使用当前WebView处理跳转
+                return true;// true表示此事件在此处被处理，不需要再广播
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                // 有页面跳转时被回调
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                // 页面跳转结束后被回调
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode,
+                                        String description, String failingUrl) {
+                // 出错
+            }
+        });
 
         mWebView.loadUrl(mUrl);
     }
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
-            mWebView.goBack();
-            return true;
-        }
-        return false;
-    }
 }
